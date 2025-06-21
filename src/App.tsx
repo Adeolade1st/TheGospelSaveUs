d15704002a6651c5194e9a4d8dfc9105480cf14e
Ilavbare Adeola <raisingsrs@gmail.com>
2025-06-21 02:57:05 -0700

    Reposition About Section After Hero

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,9 @@ import React from 'react';
 import { LanguageProvider } from './contexts/LanguageContext';
 import Header from './components/Header';
 import Hero from './components/Hero';
+import AboutSection from './components/AboutSection';
 import ContentSection from './components/ContentSection';
 import DonationSection from './components/DonationSection';
-import AboutSection from './components/AboutSection';
 import Newsletter from './components/Newsletter';
 import Footer from './components/Footer';
 
@@ -15,9 +15,9 @@ function App() {
         <Header />
         <main>
           <Hero />
+          <AboutSection />
           <ContentSection />
           <DonationSection />
-          <AboutSection />
           <Newsletter />
         </main>
         <Footer />
