fa74a8050748ec7620898a755c4ce6807ff5ccdc
Ilavbare Adeola <raisingsrs@gmail.com>
2025-07-03 18:50:22 +0100

    Updated index.ts

--- supabase/functions/stripe-webhook/index.ts
+++ supabase/functions/stripe-webhook/index.ts
@@ -2,9 +2,7 @@ import { serve } from 'https://deno.land/std@0.168.0/http/server.ts'
 import Stripe from 'https://esm.sh/stripe@14.21.0'
 import { createClient } from 'https://esm.sh/@supabase/supabase-js@2'
 
-const stripe = new Stripe(Deno.env.get('STRIPE_SECRET_KEY') || '', {
-  apiVersion: '2023-10-16',
-})
+const stripe = new Stripe(Deno.env.get('STRIPE_SECRET_KEY') || '', { apiVersion: '2023-10-16', cryptoProvider: Stripe.createSubtleCryptoProvider() })
 
 const supabase = createClient(
   Deno.env.get('SUPABASE_URL') ?? '',
