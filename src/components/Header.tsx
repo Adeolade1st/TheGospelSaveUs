86667a151aa9eca8e01f1d3e8597e8d2d12e4065
Ilavbare Adeola <raisingsrs@gmail.com>
2025-08-23 20:55:19 +0100

    Updated Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -82,7 +82,7 @@ const Header: React.FC = () => {
                 <img 
                   src="/logo tgs.png" 
                alt="God Will Provide Outreach Ministry" 
-                  className="w-auto h-1 object-contain"
+                  className="w-auto h-4 object-contain"
                   style={{ 
                     backgroundColor: 'transparent',
                     imageRendering: 'crisp-edges'
